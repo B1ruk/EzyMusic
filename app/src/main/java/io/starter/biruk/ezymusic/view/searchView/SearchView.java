05f65b33cf2040c9d1dc9f533cc42c1a8eec3e44
Biruk Tesfaw <Biruk3870@gmail.com>
2018-07-11 13:49:49 +0300

    Add searchView & SearchActivity

---
+++ app/src/main/java/io/starter/biruk/ezymusic/view/searchView/SearchView.java
@@ -0,0 +1,19 @@
+package io.starter.biruk.ezymusic.view.searchView;
+
+import java.util.List;
+
+import io.starter.biruk.ezymusic.model.entity.Song;
+
+/**
+ * Created by biruk on 11/07/18.
+ */
+
+public interface SearchView {
+    void displaySearchResults(List<Song> songs);
+
+    void displayEmptySearchResult();
+
+    void hideSearchInfo();
+
+    void hideSearchResult();
+}
