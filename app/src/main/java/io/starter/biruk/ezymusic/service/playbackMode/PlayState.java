9976cb93c8ee3d7aa8a5744fb522cb4e4ccdb116
Biruk Tesfaw <Biruk3870@gmail.com>
2018-07-11 12:05:15 +0300

    Replace the enum with a static constants
    -That way it will be easy to add it to intent filter and intent

--- app/src/main/java/io/starter/biruk/ezymusic/service/playbackMode/PlayState.java
+++ app/src/main/java/io/starter/biruk/ezymusic/service/playbackMode/PlayState.java
@@ -4,6 +4,10 @@ package io.starter.biruk.ezymusic.service.playbackMode;
  * Created by biruk on 11/07/18.
  */
 
-public enum PlayState {
-    PLAY_PAUSE, NEXT, PREVIOUS;
+public class PlayState {
+    public static final String PLAY_PAUSE="io.starter.biruk.ezymusic.service.playbackMode.play_pause";
+    public static final String NEXT="io.starter.biruk.ezymusic.service.playbackMode.next";
+    public static final String PREVIOUS="io.starter.biruk.ezymusic.service.playbackMode.previous";
+
 }
+
